3bd32ecaa9a280749bfd64f21f5f5d8fafdf1f52
enbre <noahbrezel@gmail.com>
2022-03-07 17:09:49 -0800

    adds initial dummy data, fixes icons

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("insanity check")
+
+const appboy = require('@braze/web-sdk');
+
+document.getElementById('pay').onclick =()=> {
+   // alert('you clicked this')
+   console.log('stop clicking this button!!!!')
+}
