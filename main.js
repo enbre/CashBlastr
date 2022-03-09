d98232ac72be1bd8bd71b8ed964abd5f35076d72
enbre <noahbrezel@gmail.com>
2022-03-09 10:59:12 -0800

    starts building out dynamic activity feed

--- main.js
+++ main.js
@@ -1,60 +1,49 @@
 console.log("insanity check")
 // const serverUrl = "http://localhost:3000/users"
-let user;
+
+// var requirejs =require("requirejs");
+// requirejs.config({
+//    nodeRequire: require
+// });
+
 const  populate = async () =>{
    const requestURL = './server.json'
    const request = new Request(requestURL);
    const response = await fetch (request);
-   user = await response.json();
+   const user = await response.json();
    // console.log(user)
-   document.getElementById('username').innerHTML = `@${user.username}`;
+   document.getElementById('username').innerHTML = `@${user.userName}`;
    document.getElementById('profilePic').src = `./assets/${user.profilePic}`;
-   document.getElementById('greeting').innerHTML = `Hi, ${user.firstname}`;
+   document.getElementById('greeting').innerHTML = `Hi, ${user.firstName}`;
    document.getElementById('balance').innerHTML = `$${user.balance} in CashBlastr`;
+   document.getElementById('main-feed').innerHTML = 
+   // <img src="./assets${user.pendingActivity[0].sentFrom.profilePic}" alt="Profile Pic">
+   // <img src="./assets${user.pendingActivity[0].sentTo.profilePic}" alt="Profile Pic">
+      `<h4> Pending activity:</h4>
+      <div class="main-feed-entry">
+
+      <p> ${user.pendingActivity[0].sentFrom.userName} paid ${user.pendingActivity[0].sentTo.userName}</p>
+
+      <p>${user.pendingActivity[0].comments}</p>
+      <span><i class="fas fa-heart"></i> &nbsp <i class="fas fa-comment"></i></span>
+      <br>
+      &nbsp
+   </div>`;
+
+
+
 }
 populate()
 
-// const username = 
-// const populateSidebar = (obj) => {
-//    const
-// }
 
 
-// let user;
-// const request = new XMLHttpRequest();
-// request.open("GET", "./server.json")
-// request.onload = function () {
-//    console.log(request.status)
-//    if (request.status >= 200 & request.status < 400) {
-//       user = JSON.parse(request.responseText)
-//    } else console.log('No data found')
-// }
 
-// request.onerror = function (){
-//    console.log("Connection error")
-// }
-// // renderHtml(user)
-// request.send();
 
-// const renderHtml = (user)=>{
 
-// }
 // import appboy from '@braze/web-sdk';
-// const appboy = require("@braze/web-sdk");
+// const appboy = requirejs("@braze/web-sdk");
 
-// module.exports = function(){
-//    const faker = require("faker");
-//    const _ = require("lodash")
-//    return {
-//       users: _.times(20), function (n) {
-//          return {
-//             id: n,
-//             name: faker.name.findName(),
-//             profilePic: faker.internet.avatar()
-//          }
-//       }
-//    }
-// }
+
 
 document.getElementById('pay').onclick = () => {
    console.log('stop clicking this button!!!!')
